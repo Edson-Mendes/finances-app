206dc09e610a87b6ef3d81cf8a09fea83e2488c2
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-09-25 16:43:34 -0300

    feat: add transaction card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransactionsComponent } from './page/transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'transactions', 
+    component: TransactionsComponent,
+    title: 'Transactions'
+  },
+  {
+    path: '**',
+    redirectTo: 'transactions'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionsComponent } from './page/transactions/transactions.component';
+import { TransactionCardComponent } from './component/transaction-card/transaction-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TransactionsComponent, TransactionCardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
