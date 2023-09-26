c860b725aa458a882b3c092c9ad90967e6a1de1d
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-09-26 10:13:38 -0300

    feat: tornar transaction-card dinâmico

--- src/app/component/transaction-card/transaction-card.component.ts
+++ src/app/component/transaction-card/transaction-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-transaction-card',
   templateUrl: './transaction-card.component.html',
-  styleUrls: ['./transaction-card.component.css', ]
+  styleUrls: ['./transaction-card.component.css'],
 })
 export class TransactionCardComponent {
-
+  @Input({required: true}) transactionName: string = '';
+  @Input({required: true}) transactionValue: string = '';
+  @Input({required: true}) transactionType: string = '';
 }
